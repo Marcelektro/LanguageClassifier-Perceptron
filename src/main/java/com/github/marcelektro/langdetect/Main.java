95eb450ef5230bb822d6e22f2cc65185977ef2cf
Marcelektro <marcelektro17@gmail.com>
2025-04-07 00:42:59 +0200

    Implemented console interactive interface

--- src/main/java/com/github/marcelektro/langdetect/Main.java
+++ src/main/java/com/github/marcelektro/langdetect/Main.java
@@ -20,23 +20,43 @@ public class Main {
 
         var s = new Scanner(System.in);
         while (!Thread.currentThread().isInterrupted()) {
+
+            System.out.println("Enter text to classify, or file (file://<path>) (or 'exit' to quit):");
+
             var input = s.nextLine();
 
             if (input.equals("exit")) {
+                System.out.println("Exiting...");
                 break;
+            }
+
+            if (input.startsWith("file://")) {
+                var filePath = input.substring(7);
+                var file = new File(filePath);
+                if (!file.exists()) {
+                    System.out.println("File does not exist! Are you sure you provided the correct path?");
+                    continue;
+                }
+                System.out.println("Classifying file: " + file.getAbsolutePath());
+                try {
+                    var fileContents = LangClassifier.readStringFromFile(file, Integer.MAX_VALUE);
+
+                    System.out.println("File contents length: " + fileContents.length());
+
+                    var result = classifier.classify(fileContents);
+                    System.out.println("Detected language: " + result);
+
+                } catch (Exception e) {
+                    System.out.println("Error reading file: " + e.getMessage());
+                }
+
+                continue;
             }
 
             var result = classifier.classify(input);
             System.out.println("Detected language: " + result);
 
-
         }
-
-
-        /*var testStr = "Welcome to another video on my channel!";
-        var result = classifier.classify(testStr);
-
-        System.out.println("Detected language: " + result);*/
 
     }
 
